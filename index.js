04af2dc03c4f0412636564e9698bd2e8be30dd32
gracesuhyun <gracesuhyun@gmail.com>
2022-08-03 00:06:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ let auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport'); 
 
-mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://testuser:TestUser@myflixdb.d5y2zjb.mongodb.net/test', { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 // READ
